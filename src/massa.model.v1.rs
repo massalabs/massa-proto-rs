f0bde75bac7b3e9f3625b6c711d3a5a6b1d7c0ac
Moncef AOUDIA <ma@massa.net>
2023-07-10 14:15:42 +0200

    Revert "fix: remove async message id"
    This reverts commit be07ea5800cda58e5772f4401ad472da7633fa76.

--- src/massa.model.v1.rs
+++ src/massa.model.v1.rs
@@ -780,7 +780,7 @@ pub struct StateChanges {
     pub ledger_changes: ::prost::alloc::vec::Vec<LedgerChangeEntry>,
     /// Asynchronous pool changes
     #[prost(message, repeated, tag = "2")]
-    pub async_pool_changes: ::prost::alloc::vec::Vec<AsyncPoolChange>,
+    pub async_pool_changes: ::prost::alloc::vec::Vec<AsyncPoolChangeEntry>,
     /// Executed operations changes
     #[prost(message, repeated, tag = "4")]
     pub executed_ops_changes: ::prost::alloc::vec::Vec<ExecutedOpsChangeEntry>,
@@ -810,19 +810,30 @@ pub struct ExecutedOpsChangeValue {
     #[prost(message, optional, tag = "2")]
     pub slot: ::core::option::Option<Slot>,
 }
-/// AsyncPoolChange
+/// AsyncPoolChange Entry
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
-pub struct AsyncPoolChange {
+pub struct AsyncPoolChangeEntry {
+    /// Async message id
+    #[prost(string, tag = "1")]
+    pub async_message_id: ::prost::alloc::string::String,
+    /// AsyncPool message
+    #[prost(message, optional, tag = "2")]
+    pub value: ::core::option::Option<AsyncPoolChangeValue>,
+}
+/// AsyncPoolChangeValue
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct AsyncPoolChangeValue {
     /// The type of the change
     #[prost(enumeration = "AsyncPoolChangeType", tag = "1")]
     pub r#type: i32,
     /// AsyncPool message
-    #[prost(oneof = "async_pool_change::Message", tags = "2, 3")]
-    pub message: ::core::option::Option<async_pool_change::Message>,
+    #[prost(oneof = "async_pool_change_value::Message", tags = "2, 3")]
+    pub message: ::core::option::Option<async_pool_change_value::Message>,
 }
-/// Nested message and enum types in `AsyncPoolChange`.
-pub mod async_pool_change {
+/// Nested message and enum types in `AsyncPoolChangeValue`.
+pub mod async_pool_change_value {
     /// AsyncPool message
     #[allow(clippy::derive_partial_eq_without_eq)]
     #[derive(Clone, PartialEq, ::prost::Oneof)]
