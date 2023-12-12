84678fb77cf6d06d85d55e2c20502908ff292e61
massabot <info@massa.net>
2023-12-12 13:33:40 +0000

    Apply automatic changes

--- src/massa.abi.v1.rs
+++ src/massa.abi.v1.rs
@@ -129,7 +129,7 @@ pub struct FunctionExistsResult {
 pub struct RespResult {
     #[prost(
         oneof = "resp_result::Res",
-        tags = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62"
+        tags = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63"
     )]
     pub res: ::core::option::Option<resp_result::Res>,
 }
@@ -262,6 +262,8 @@ pub mod resp_result {
         EvmGetPubkeyFromSignatureResult(super::EvmGetPubkeyFromSignatureResult),
         #[prost(message, tag = "62")]
         IsAddressEoaResult(super::IsAddressEoaResult),
+        #[prost(message, tag = "63")]
+        ChainIdResult(super::ChainIdResult),
     }
 }
 /// Generic message that encapsulate response from ABI calls.
@@ -1266,4 +1268,15 @@ pub struct Base58CheckToBytesRequest {
 pub struct Base58CheckToBytesResult {
     #[prost(bytes = "vec", tag = "1")]
     pub bytes: ::prost::alloc::vec::Vec<u8>,
+}
+/// Chain id request
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ChainIdRequest {}
+/// Chain id result
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ChainIdResult {
+    #[prost(uint64, tag = "1")]
+    pub id: u64,
 }
